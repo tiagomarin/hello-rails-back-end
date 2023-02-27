5914babcee57eef4c4e3fcf8e24425e2d8b01418
Tiago Marin <tiago.lelinski@gmail.com>
2023-02-27 13:04:37 -0300

    Udpate Reame - Installed Linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,5 +2,5 @@ class Api::V1::GreetingsController < ApplicationController
   # get a random greeting message from the Database
   def message
     render json: Greeting.all.sample
-  end 
+  end
 end
